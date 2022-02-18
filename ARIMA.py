730e7213c72014c9ba8080c2b8ad2a1545860d11
Luke Li <lta19@mails.tsinghua.edu.cn>
2022-02-18 16:11:12 +0800

    Update ARIMA.py

--- ARIMA.py
+++ ARIMA.py
@@ -13,30 +13,89 @@ plt.rcParams.update({'font.size': 12})
 plt.style.use('ggplot')
 
 class ARIMA:
-    def __init__(self,gold_path,bit_path):
+    def __init__(self,gold_path,bitcoin_path):
         """initialization"""
         self.gold_path = gold_path
-        self.bit_path = bit_path
+        self.bitcoin_path = bitcoin_path
         self.gold_data = pd.read_csv(gold_path, engine='python', skipfooter=3)
-        self.bit_data = pd.read_csv(bit_path, engine='python', skipfooter=3)
+        self.bitcoin_data = pd.read_csv(bitcoin_path, engine='python', skipfooter=3)
+        self.gold_data['Month']=pd.to_datetime(self.gold_data['Month'], format='%Y-%m-%d')
+        self.gold_data.set_index(['Month'], inplace=True)
+        self.bitcoin_data['Month']=pd.to_datetime(self.bitcoin_data['Month'], format='%Y-%m-%d')
+        self.bitcoin_data.set_index(['Month'], inplace=True)
+        print("\nLoad data success!\n")
+        
 
     def plot_data(self):
         """plot the data from csv files"""
+        print("\nplotting data\n")
         # plot gold data
-        plt.figure(1)
         self.gold_data.plot()
-        plt.ylabel('Monthly airline passengers (x1000)')
+        plt.ylabel('Gold daily prices')
         plt.xlabel('Date')
 
         # plot bit coin data
-        plt.figure(2)
-        self.bit_data.plot()
-        plt.ylabel('Monthly airline passengers (x1000)')
+        self.bitcoin_data.plot()
+        plt.ylabel('Bitcoin daily prices')
         plt.xlabel('Date')
         plt.show()
 
 
-    def predict(self,train_start,train_end,test_end):
+    def predict(self,label = 'gold',train_start='2016-9-12',train_end='2018-9-12'):
         """predict preices using ARIMA"""
-        result = {}
+        # prepare data
+        if label == 'gold':
+            train_data = self.gold_data[train_start:train_end]
+        elif label == 'bitcoin':
+            train_data = self.bitcoin_data[train_start:train_end]
+        else:
+            print('\nWrong label!\n')
+            return
+
+        # Define the d and q parameters to take any value between 0 and 1
+        q = d = range(0, 2)
+        # Define the p parameters to take any value between 0 and 3
+        p = range(0, 4)
+        # Generate all different combinations of p, q and q triplets
+        pdq = list(itertools.product(p, d, q))
+        # Generate all different combinations of seasonal p, q and q triplets
+        seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+        print('Examples of parameter combinations for Seasonal ARIMA...')
+        print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+        print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+        print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+        print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+        warnings.filterwarnings("ignore") # specify to ignore warning messages
+        AIC = []
+        SARIMAX_model = []
+        for param in pdq:
+            for param_seasonal in seasonal_pdq:
+                try:
+                    mod = sm.tsa.statespace.SARIMAX(train_data,
+                                                    order=param,
+                                                    seasonal_order=param_seasonal,
+                                                    enforce_stationarity=False,
+                                                    enforce_invertibility=False)
+
+                    results = mod.fit()
+
+                    print('SARIMAX{}x{} - AIC:{}'.format(param, param_seasonal, results.aic), end='\r')
+                    AIC.append(results.aic)
+                    SARIMAX_model.append([param, param_seasonal])
+                except:
+                    continue
+        print('The smallest AIC is {} for model SARIMAX{}x{}'.format(min(AIC), SARIMAX_model[AIC.index(min(AIC))][0],SARIMAX_model[AIC.index(min(AIC))][1]))
+        results.plot_diagnostics(figsize=(20, 14))
+        plt.show()
         return result
+
+
+
+
+if __name__ == "__main__":
+    arima_model = ARIMA(gold_path="./2022_Problem_C_DATA/LBMA-GOLD.csv",
+                        bitcoin_path="./2022_Problem_C_DATA/BCHAIN-MKPRU.csv")
+    # arima_model.plot_data()
+    arima_model.predict(label='gold',train_start='2016-9-12',train_end='2018-9-12')
